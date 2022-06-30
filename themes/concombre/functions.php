8427925ac73f46fab3dac1b9f57a60fd1c2d7441
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-30 16:54:46 +0200

    ADD : Menu dynamique wp_navbar

--- themes/concombre/functions.php
+++ themes/concombre/functions.php
@@ -3,13 +3,39 @@
 // ------------------------------------------------ 
 // 1 – Les Constantes 
 // ------------------------------------------------
-define( 'THEMEROOT', get_stylesheet_directory_uri() ); 
-define( 'IMG', THEMEROOT. '/img' ); 
-define( 'JS', THEMEROOT. '/js' ); 
-define( 'CSS', THEMEROOT. '/css' ); 
+define('THEMEROOT', get_stylesheet_directory_uri());
+define('IMG', THEMEROOT . '/img');
+define('JS', THEMEROOT . '/js');
+define('CSS', THEMEROOT . '/css');
 
 function my_theme_setup()
 {
     // active les images mises en avant dans Article (Post)
-//    add_theme_support('post-thumbnails');
+    //    add_theme_support('post-thumbnails');
+    register_nav_menus([
+        // 'identifiant' => 'Nom dans le backoffice Wordpress',
+        'menu-haut' => 'Menu en haut',
+        'menu-bas' => 'Menu en bas'
+    ]);
 }
+
+add_action('after_setup_theme', 'my_theme_setup');
+
+// ------------------------------------------------
+//-------------------------------------------------------
+// 3 . Ajouter les class css au menu Bootstrap 5 
+//-------------------------------------------------------
+// <ul><li><a class="nav-link"
+// <ul><li><a class="nav-link active" si page courante
+
+function my_filter_a($attributes)
+{
+    $attributes["class"] = "nav-link";
+    if ($attributes["aria-current"]) {
+        $attributes["class"] = "nav-link active";
+    }
+    return $attributes;
+}
+
+add_filter('nav_menu_link_attributes', 'my_filter_a');
+//-------------------------------------------------------
--- themes/concombre/header.php
+++ themes/concombre/header.php
@@ -27,24 +27,6 @@
     </title>
 
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container px-5">
-            <a class="navbar-brand" href="<?php echo get_home_url() ?>/"> <img class="icon" src="<?= get_site_icon_url() ?>" width="25"><?php
-                                                                                                                            bloginfo('name');
-                                                                                                                            ?></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo get_home_url() ?>/">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo get_home_url() ?>/about-us">About </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php the_permalink(21) ?>">Contact</a></li>
-
-                </ul>
-
-            </div>
-        </div>
-    </nav>
 
     <!-- wp peut ajouter css ou js -->
     <?php wp_head() ?>
@@ -52,3 +34,33 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="<?php echo get_home_url() ?>/"> <img class="icon" src="<?= get_site_icon_url() ?>" width="25"><?php
+                                                                                                                                        bloginfo('name');
+                                                                                                                                        ?></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <!-- <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo get_home_url() ?>/">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_home_url() ?>/about-us">About </a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php the_permalink(21) ?>">Contact</a></li>
+                </ul> -->
+                <div class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <?php
+                    $menuParameters = array(
+                        'theme_location' => 'menu-haut',
+                        // 'menu_class' => 'text-light text-center',
+                        'container' => false,
+                        'echo' => '', // try: false
+                        'items_wrap' => '%3$s',
+                    );
+                    echo strip_tags(wp_nav_menu($menuParameters), '<a>');
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <nav>
+    </nav>
